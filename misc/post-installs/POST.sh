f6b7d27ebde70c63674c3de18dc8e5b6b2704b2b
cruxppc <cruxppc@gmail.com>
2022-11-26 14:17:04 +0000

    Added initial makelocales and post install helpers

---
+++ misc/post-installs/POST.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# echo "running selected post-inst alls"
+(pkginfo -i |grep '^adwaita-icon-theme ') &&  (/bin/bash -v adwaita-icon-theme/post-install)
+(pkginfo -i |grep '^dbus ')  &&  (/bin/bash -v dbus/post-install)
+(pkginfo -i |grep '^glib ')  &&  (/bin/bash -v glib/post-install)
+(pkginfo -i |grep '^gtk3 ')  &&  (/bin/bash -v gtk3/post-install)
+(pkginfo -i |grep '^libglade ')  &&  (/bin/bash -v libglade/post-install)
+(pkginfo -i |grep '^libinput ')  &&  (/bin/bash -v libinput/post-install)
+(pkginfo -i |grep '^librsvg ')  &&  (/bin/bash -v librsvg/post-install)
+(pkginfo -i |grep '^pcmanfm ')  &&  (/bin/bash -v pcmanfm/post-install)
+(pkginfo -i |grep '^pulseaudio ')  &&  (/bin/bash -v pulseaudio/post-install)
+(pkginfo -i |grep '^xorg-font-misc-misc ')  &&  (/bin/bash -v xorg-font-misc-misc/post-install)
